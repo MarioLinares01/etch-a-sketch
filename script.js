43265c9ef6b7aba5768433acee6caa759dfa4e73
Mario Linares <mariolinares109@gmail.com>
2024-01-29 09:20:56 -0800

    Add function to resize the board

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    defaultGrid();
+    createBoard();
+    resizeBoard();
 });
 
 // Create the inital board: 16x16
-function createGrid(size = 16) {
+function createBoard(size = 16) {
     let gridSize = size * size
     let board = document.querySelector('.board'); 
     board.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
@@ -11,8 +12,20 @@ function createGrid(size = 16) {
     for (let i = 0; i < gridSize; i++) {
         let grid = document.createElement('div');
         grid.style.border = '1px solid #9f9f9f';
+        grid.style.height = '100%';
         board.appendChild(grid);
     }
 }
 
-// Resize grid
+// Function to resize the Grid
+function resizeBoard() {
+    const resizeButton = document.querySelector('.resize-button');
+    let board = document.querySelector('.board'); 
+    resizeButton.addEventListener('click', function(e) {
+        let boardSize = prompt("How many grids per row and column would you like?")
+        while (board.firstChild) {
+            board.removeChild(board.firstChild);
+        }
+        createBoard(boardSize);
+    }) 
+}
